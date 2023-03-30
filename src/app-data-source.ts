3dd6f157fc0a4b7107c87955fdcc26d9736daccb
De <deehzi@pm.me>
2023-03-30 13:15:45 -0300

    feat(app-data-source): adding variables to the .env

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,12 +1,21 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port: number = parseInt(process.env.BDPORT as string) || 5432;
+const host = process.env.BDHOST;
+const password = process.env.BDPASSWORD;
+const username = process.env.BDUSERNAME;
+const database = process.env.BDDATABASE;
 
 const PostgresDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1618',
-  database: 'postgres',
+  host: host,
+  port: port,
+  username: username,
+  password: password,
+  database: database,
   entities: [
     // ....
   ],

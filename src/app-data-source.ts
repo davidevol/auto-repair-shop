26829674fcf4fcbb3731de0c24e97eeda10fa037
De <deehzi@pm.me>
2023-03-31 17:38:49 -0300

    feat(app-data-source): adding ClientRequestEntity

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,24 +1,20 @@
 import { DataSource } from 'typeorm';
+import 'reflect-metadata';
 import dotenv from 'dotenv';
+import { ClientRequestEntity } from './entity/client/clientRequestEntity';
 
 dotenv.config();
 
-const port: number = parseInt(process.env.BDPORT as string) || 5432;
-const host = process.env.BDHOST;
-const password = process.env.BDPASSWORD;
-const username = process.env.BDUSERNAME;
-const database = process.env.BDDATABASE;
-
 const PostgresDataSource = new DataSource({
   type: 'postgres',
-  host: host,
-  port: port,
-  username: username,
-  password: password,
-  database: database,
-  entities: [
-    // ....
-  ],
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'postgres',
+  // entities: [__dirname + '/entity/*{.js,.ts}'], didn't work =(
+  entities: [ClientRequestEntity],
+  synchronize: true,
 });
 
 export default PostgresDataSource;

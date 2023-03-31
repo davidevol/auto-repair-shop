0de200a55a6234b9caaebe6a995091a019c19c98
De <deehzi@pm.me>
2023-03-31 17:29:24 -0300

    feat(app): adding a req handler for non api routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, RequestHandler } from 'express';
+import 'reflect-metadata';
 import bodyParser from 'body-parser';
-import defaultRouter from './routes/default';
+import defaultRouter from './routes/defaultRouter';
 
 const app: Application = express();
 
@@ -8,6 +9,16 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '1.5MB' }));
 
+const catchNonApiRoutes: RequestHandler = (req, res, next) => {
+  const { path } = req;
+  if (!path.startsWith('/api/v1/')) {
+    next(new Error(`Unknown route: ${path}`));
+  } else {
+    next();
+  }
+};
+
+app.use(catchNonApiRoutes);
 app.use(defaultRouter);
 
 export default app;

d31905a6ec6472c3531bfa53021a2426431ab8e9
De <deehzi@pm.me>
2023-04-02 23:06:15 -0300

    fix(part): fixing the export class name

--- src/entity/part/partListResponseEntity.ts
+++ src/entity/part/partListResponseEntity.ts
@@ -2,7 +2,7 @@ import { Entity } from 'typeorm';
 import { PartResponseEntity } from './partResponseEntity';
 
 @Entity()
-export class ClientListResponseEntity {
+export class PartListResponseEntity {
   limit!: number;
   offset!: number;
   total!: number;

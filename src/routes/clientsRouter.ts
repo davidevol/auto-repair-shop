f1deba1077ea1b2d1cdfe184b3c20805c2dc7f45
De <deehzi@pm.me>
2023-04-03 16:19:42 -0300

    feat: adding error handling to the client routes

--- src/routes/clientsRouter.ts
+++ src/routes/clientsRouter.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import ClientController from '../controllers/clientController';
+import { QueryFailedError } from 'typeorm';
+import {
+  ValidationErrorResponse,
+  validationErrorHandler,
+} from '../app-validation-error';
+import { AppError } from '../app-error';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.use(
+  (
+    err: ValidationErrorResponse,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    err instanceof QueryFailedError
+      ? validationErrorHandler(err, req, res, next)
+      : next(new AppError(500, 'Internal server error', 'error'));
+  },
+);
+
+router.get('/', async (req: Request, res: Response) => {
   const limit = req.query.limit;
   const offset = req.query.offset;
   const controller = new ClientController();
@@ -11,33 +30,46 @@ router.get('/', async (req, res) => {
   return res.send(response);
 });
 
-router.post('/', async (req, res) => {
-  const controller = new ClientController();
-  const response = await controller.createClients(req.body);
-  return res.send(response);
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const controller = new ClientController();
+    const response = await controller.createClients(req.body);
+    return res.send(response);
+  } catch (err) {
+    err instanceof QueryFailedError;
+    return next(err);
+  }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
   const controller = new ClientController();
   const response = await controller.getClient(req.params.id);
-  if (!response) res.status(404).send({ message: 'No user found' });
-  return res.send(response);
-});
-
-router.patch('/:id', async (req, res) => {
-  const controller = new ClientController();
-  const response = await controller.updateClient(req.params.id, req.body);
-  if (!response) res.status(404).send({ message: 'No user found' });
-  return res.send(response);
-});
-
-router.delete('/:id', async (req, res) => {
-  const controller = new ClientController();
-  const response = await controller.deleteClient(req.params.id);
   return !response
-    ? res.status(404).send({ message: 'No user found' })
-    : res.status(200).send({ message: 'User deleted successfully' });
+    ? next(new AppError(404, `Unknown user ID ${req.params.id}`, 'Not Found'))
+    : res.send(response);
 });
+
+router.patch(
+  '/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const controller = new ClientController();
+    const response = await controller.updateClient(req.params.id, req.body);
+    return !response
+      ? next(new AppError(404, `Unknown user ID ${req.params.id}`, 'Not Found'))
+      : res.send(response);
+  },
+);
+
+router.delete(
+  '/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const controller = new ClientController();
+    const response = await controller.deleteClient(req.params.id);
+    return !response
+      ? next(new AppError(404, `Unknown user ID ${req.params.id}`, 'Not Found'))
+      : res.status(204);
+  },
+);
 
 // TODO
 

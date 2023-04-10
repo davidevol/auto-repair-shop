936ffdeecee7387c2b31e9343e4476d9220c5063
De <deehzi@pm.me>
2023-04-09 21:24:21 -0300

    feat(middleware): improving the scope of the non-route handler

--- src/middlewares/catchNonApiRoutesMiddleware.ts
+++ src/middlewares/catchNonApiRoutesMiddleware.ts
@@ -3,7 +3,7 @@ import { AppError } from '../errors/appError';
 
 export const catchNonApiRoutesMiddleware: RequestHandler = (req, res, next) => {
   const { path } = req;
-  !path.startsWith('/api/v1/')
+  !path.startsWith('*')
     ? next(new AppError(404, `Unknown route: ${path}`, 'Not Found'))
     : next();
 };

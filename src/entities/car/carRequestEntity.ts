7442cab9a55629529e7e2ccfde5476219eedd5fe
De <deehzi@pm.me>
2023-04-10 11:25:17 -0300

    feat(car): adding a decorator to accept uuid as a parameter

--- src/entities/car/carRequestEntity.ts
+++ src/entities/car/carRequestEntity.ts
@@ -1,28 +1,33 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class CarResponseEntity {
+export class CarRequestEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
   @Column()
   @IsString()
+  @IsUUID()
   license_plate!: string;
 
   @Column()
   @IsString()
+  @IsNotEmpty({ message: 'The model cannot be empty.' })
   model!: string;
 
   @Column()
   @IsNumber()
+  @IsNotEmpty({ message: 'The year cannot be empty.' })
   year!: number;
 
   @Column()
   @IsString()
+  @IsNotEmpty({ message: 'The manufacturer cannot be empty.' })
   manufacturer!: string;
 
   @Column()
   @IsString()
+  @IsNotEmpty({ message: 'The color cannot be empty.' })
   color!: string;
 }
